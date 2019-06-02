f0724b595d0344ef2880351669d075dfc9525218
Sula Gheorghe <sula.gheorghe94@gmail.com>
2019-06-02 21:22:12 +0300

    service add

--- server/src/student/student.service.ts
+++ server/src/student/student.service.ts
@@ -3,38 +3,55 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Student } from './student.entity';
 import { Repository } from 'typeorm';
 import { CreateStudentDTO } from './dto/create-student.dto';
+import { GradeGroup } from './grade-group.entity';
 
 @Injectable()
 export class StudentService {
-    constructor(
-        @InjectRepository(Student) private readonly studentRepo: Repository<Student>,
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepo: Repository<Student>,
+  ) {}
 
-    ){ }
+  public async createFromDTO(
+    studentCreateDTO: CreateStudentDTO,
+  ): Promise<Student> {
+    const student = new Student(
+      studentCreateDTO.firstname,
+      studentCreateDTO.lastname,
+      studentCreateDTO.email,
+      studentCreateDTO.gradeGroup,
+    );
+    return await this.studentRepo.save(student);
+  }
 
-    public async createFromDTO(studentCreateDTO: CreateStudentDTO): Promise<Student> {
-        const student = new Student(
-            studentCreateDTO.firstname,
-            studentCreateDTO.lastname,
-            studentCreateDTO.email,
-            studentCreateDTO.gradeGroup
-            );
-        return await this.studentRepo.save(student);
+  public async getById(id: number, includeGrade = false): Promise<Student> {
+    let findOptions = { relations: [] };
+    if (includeGrade) {
+      findOptions.relations = ['gradeGroup'];
     }
+    return await this.studentRepo.findOne(id, findOptions);
+  }
 
-    public async getById(id: number, includeGrade = false): Promise<Student> {
-        let findOptions = {relations: []}
-        if (includeGrade) {
-            findOptions.relations = ['gradeGroup'];
-        }
-        return await this.studentRepo.findOne(id, findOptions);
-    }
+  public async update(
+    toBeUptated: Student,
+    source: CreateStudentDTO,
+  ): Promise<Student> {
+    this.studentRepo.merge(toBeUptated, source);
+    return await this.studentRepo.save(toBeUptated);
+  }
 
-    public async update(toBeUptated: Student, source: CreateStudentDTO): Promise<Student> {
-        this.studentRepo.merge(toBeUptated, source);
-        return await this.studentRepo.save(toBeUptated);
-    }
+  public async getAll(): Promise<Student[]> {
+    return await this.studentRepo.find({ relations: ['gradeGroup'] });
+  }
 
-    public async getAll(): Promise<Student[]> {
-        return await this.studentRepo.find({relations: ['gradeGroup']});
-    }
+  public async getGradeGroupsStudent(
+    gradeGroup: GradeGroup,
+  ): Promise<Student[]> {
+    return await this.studentRepo.find({
+      where: {
+        gradeGroup: gradeGroup.id,
+      },
+      relations: ['gradeGroup'],
+    });
+  }
 }
