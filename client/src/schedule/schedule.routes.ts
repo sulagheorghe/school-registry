97ecb5bab0d73a3156493488cc9d6b3a68aecd58
Andrew Luca <thendrluca@gmail.com>
2019-06-02 13:29:16 +0300

    feat(client): adapt schedule

--- client/src/schedule/schedule.routes.ts
+++ client/src/schedule/schedule.routes.ts
@@ -1,6 +1,6 @@
 import {createRoute} from '../utils/create-route';
 
 export const scheduleRoutes = {
-    list:createRoute('/schedule'),
-    view:createRoute('/schedule/grade-group/:id')
+    list: createRoute('/schedule'),
+    view: createRoute('/schedule/grade-group/:id')
 }
